6dd56bbcf96264f9af8fdcd55742774660386d5f
Alessandro Astone <ales.astone@gmail.com>
2025-05-23 18:36:57 +0200

    arm64_only: Enable TARGET_SUPPORTS_OMX_SERVICE
    This is implicitly true, but the arm64_only makefiles complain:
    
    build/make/target/product/core_64_bit_only.mk:35: warning: TARGET_SUPPORTS_OMX_SERVICE is undefined. Please set it to true if your target uses OMX service, or false otherwise.
    
    We need OMX because codec2 only handles YV12 buffers.

--- waydroid_arm64_only/lineage_waydroid_arm64_only.mk
+++ waydroid_arm64_only/lineage_waydroid_arm64_only.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+TARGET_SUPPORTS_OMX_SERVICE := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 

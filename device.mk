88c07ce748f0f823365df317a3d2e65a8fee220a
cool <bilibili@att.net>
2026-03-07 01:26:09 +0800

    waydroid: Remove VNDK version override for non-x86 targets
    Remove the conditional PRODUCT_EXTRA_VNDK_VERSIONS assignment that
    was only applied to non-x86/TV targets. This is no longer needed
    for the current build configuration.

--- device.mk
+++ device.mk
@@ -325,10 +325,6 @@ $(call inherit-product-if-exists, vendor/intel/proprietary/houdini/native_bridge
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.nativebridge=1
 endif
 
-ifeq ($(filter %_waydroid_x86 %_waydroid_x86_64 %_waydroid_tv%,$(TARGET_PRODUCT)),)
-PRODUCT_EXTRA_VNDK_VERSIONS := 29 30
-endif
-
 # Updater
 PRODUCT_PACKAGES += \
     WaydroidUpdater

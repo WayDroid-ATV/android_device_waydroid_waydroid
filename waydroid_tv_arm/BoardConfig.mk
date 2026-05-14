246f418a9f746069fb5453dc3e8f3429ef2752c1
SupeChicken666 <me@supechicken666.dev>
2026-05-14 23:17:29 +0800

    Enable scudo for ARM
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- waydroid_tv_arm/BoardConfig.mk
+++ waydroid_tv_arm/BoardConfig.mk
@@ -23,9 +23,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := generic
 
-# Disable scudo
-MALLOC_SVELTE := true
-
 ifneq ($(TARGET_USE_MESA),false)
 BOARD_MESA3D_GALLIUM_DRIVERS += freedreno v3d vc4 etnaviv tegra panfrost lima
 BOARD_MESA3D_VULKAN_DRIVERS += broadcom freedreno panfrost
